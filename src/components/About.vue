ac0a1914137b12129a46973aff8776aaf08a5121
sakuramochi <sakuramochi.mochi.0@gmail.com>
2019-02-17 03:12:35 +0900

    Build the static pages for GitHub pages.

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,9 +5,9 @@
     </header>
     <h1>『キンプリラッシュ』<br />並び替えミニゲーム<br />練習アプリ</h1>
     <p>
-      このアプリは『
+      『
       <a href="https://prism-rush.com/">KING OF PRISM プリズムラッシュ！LIVE</a>
-      』の「ならびかえ」ミニゲームの練習をするためのアプリです。
+      』の「ならびかえ」ミニゲームの練習をするアプリです。
     </p>
     <p>
       <router-link to="/game">
@@ -16,6 +16,29 @@
           <span>ゲームをやってみる！</span>
         </button>
       </router-link>
+    </p>
+
+    <hr />
+
+    <p>
+      作者:
+      <a href="https://twitter.com/skrmch_rhythpri/">
+        <b-icon icon="twitter"></b-icon>
+        さくらもち (@skrmch_rhythpri)
+      </a>
+    </p>
+
+    <p>
+      <b-icon icon="note"></b-icon>
+      作ったコード:<br />
+      <a href="https://github.com/sakuramochi0/kinpri-rush-minigame-simulator">
+        github.com/sakuramochi0/kinpri-rush-minigame-simulator
+      </a>
+    </p>
+
+    <p>
+      壁紙:
+      <a href="https://pixabay.com/photo-755962/">Pixabay by Osckar</a>
     </p>
   </div>
 </template>
@@ -30,6 +53,10 @@ export default {
 </script>
 
 <style scoped>
+a {
+  color: #0074d9;
+}
+
 header {
   padding: 60px;
   margin-bottom: 1em;
